b9ba454dcdf52299d0cacc3711f9c570df731c35
Rizaldi <64939575+Rizaldi-R@users.noreply.github.com>
2025-08-16 03:21:38 +0700

    refactor: Update ResourceNotFoundException type structure

--- src/_common/exceptions/custom-not-found.exception.ts
+++ src/_common/exceptions/custom-not-found.exception.ts
@@ -1,13 +1,13 @@
 import { NotFoundException } from '@nestjs/common';
 
 // type Subject = 'User' | 'Student' | 'Instructor' | 'Account' | 'Transaction';
-type IndentifierType = 'id' | 'user id' | 'email' | 'username';
+// type IndentifierType = 'id' | 'user id' | 'email' | 'username';
 
 export class ResourceNotFoundException extends NotFoundException {
   // public status: HttpStatus;
   constructor(
     subject: string,
-    indentifierType: IndentifierType,
+    indentifierType: string,
     indentifier: number | string,
     message: string = `${subject} with ${indentifierType} ${indentifier} not found.`,
     // status: HttpStatus = HttpStatus.NOT_FOUND,
